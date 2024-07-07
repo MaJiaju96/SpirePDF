a05cdf6e66d8e8c01eb0bd4dace18aaa654f706c
majiaju <majiaju96@163.com>
2024-07-07 21:19:06 +0800

    优化: Main方法jar包打印outputStream流, 接口读取问题,重新转换成流成功

--- src/main/java/org/sample/Main.java
+++ src/main/java/org/sample/Main.java
@@ -1,79 +1,93 @@
 package org.sample;
 
-import com.spire.pdf.graphics.PdfMargins;
-import com.spire.pdf.htmlconverter.LoadHtmlType;
-import com.spire.pdf.htmlconverter.qt.HtmlConverter;
-import com.spire.pdf.htmlconverter.qt.Size;
+import com.spire.pdf.PdfDocument;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @SpringBootApplication
 public class Main {
 
-//    public static void main(String[] args) {
-//        try {
-//
-////            String htmlString = "/home/root/convertPDF/file/aaa.html";
-////            String pluginPath = "D:\\Work\\Doc\\plugins-windows-x64\\plugins";
-//
-////            htmlString = HtmlToString(htmlString);
-//
-////            String a = "Hello";
-//
-//            // 假设你的jar文件名为example.jar
-////            ProcessBuilder pb = new ProcessBuilder("java", "-jar" , "-DhtmlString=" + htmlString, "./SpirePDF-3.0.jar");
-//            ProcessBuilder pb = new ProcessBuilder("java", "-jar" , "./SpirePDF-3.0.jar");
-//            Process process = pb.start();
-//
-//            // 读取jar文件的输出
-//            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-//            String line;
-//            while ((line = reader.readLine()) != null) {
-//                System.out.println(line);
-//            }
-//
-//            // 等待jar执行完成
-//            process.waitFor();
-//
-//            // 获取jar执行的退出值
-//            int exitValue = process.exitValue();
-//            System.out.println("Exit value: " + exitValue);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
+
+    public static void main(String[] args) {
+        try {
+
+            String pluginPath = "/Users/majiaju/Work/SPD/Tools/plugins_mac";
+            String htmlString = "/Users/majiaju/Work/SPD/PDF_File/aaa.html";
+            htmlString = HtmlToString(htmlString);
+
+            String jarPath = "/Users/majiaju/Project/Learn/SpirePDF/target/SpirePDF-1.0.jar";
 
 
-    public static void main(String[] args)  {
+            // 假设你的jar文件名为example.jar
+            ProcessBuilder pb = new ProcessBuilder(
+                    "java", "-jar" ,
+                    "-DhtmlString=" + htmlString,
+                    "-DpluginPath=" + pluginPath,
+                    jarPath);
+//            ProcessBuilder pb = new ProcessBuilder("java", "-jar" , jarPath);
+            Process process = pb.start();
 
-        try{
-            String htmlString = System.getProperty("htmlString");
-            String pluginPath = System.getProperty("pluginPath");;
-            //设置插件路径
-            HtmlConverter.setPluginPath(pluginPath);
-            // 转字节流
-            OutputStream bos = new ByteArrayOutputStream();
-            //将HTML字符串转换为PDF
-            HtmlConverter.convert(htmlString, bos, true, 100000, new Size(700, 900), new PdfMargins(0), LoadHtmlType.Source_Code);
-            System.out.println(bos);
-            bos.close();
-            System.exit(0);
-        }catch (IOException e){
+            // 读取jar文件的输出
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            StringBuilder stringBuilder = new StringBuilder();
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line).append("\n");
+            }
+            reader.close();
+
+            PdfDocument pdf = new PdfDocument();
+            pdf.loadFromStream(new ByteArrayInputStream(stringBuilder.toString().getBytes(StandardCharsets.UTF_8)));
+            System.out.println(pdf);
+
+            // 等待jar执行完成
+            process.waitFor();
+            // 获取jar执行的退出值
+            int exitValue = process.exitValue();
+            System.out.println("Exit value: " + exitValue);
+        } catch (Exception e) {
             e.printStackTrace();
-            System.out.println(e.getMessage());
-            System.exit(1);
         }
-
     }
 
 
 
+
+//    public static void main(String[] args)  {
+//
+//        try{
+//            String htmlString = System.getProperty("htmlString");
+//            String pluginPath = System.getProperty("pluginPath");
+//
+//            //设置插件路径
+//            HtmlConverter.setPluginPath(pluginPath);
+//            // 转字节流
+//            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+//            //将HTML字符串转换为PDF
+//            HtmlConverter.convert(htmlString, baos, true, 100000, new Size(700, 900), new PdfMargins(0), LoadHtmlType.Source_Code);
+//            String outputString = baos.toString(StandardCharsets.UTF_8.name());
+//            baos.close();
+//            System.out.println(outputString);
+//            System.exit(0);
+//        }catch (IOException e){
+//            e.printStackTrace();
+//            System.out.println(e.getMessage());
+//            System.exit(1);
+//        }
+//
+//    }
+
+
+
+
     public static String HtmlToString(String filePath) throws IOException {
-        File file = new File(filePath);
-        FileReader fileReader = new FileReader(file);
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        String encoding = "UTF-8";
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(Files.newInputStream(Paths.get(filePath)), encoding));
+
         StringBuilder stringBuilder = new StringBuilder();
         String temp = "";
 
